6d6f95d7213393775c462b15b9130fa8161de698
Facundo Fernandez <ff.fernandez.facundo@gmail.com>
2018-08-03 20:03:34 -0300

    Add README

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -3,45 +3,9 @@ const { join } = require('path');
 const Hotels = require('../models/hotels');
 let data = require(join(__dirname, '../data/data.json'));
 
-module.exports.get = getHoteles;
 module.exports.getHotelsByName = getHotelsByName;
 module.exports.getHotelsByStars = getHotelsByStars;
 module.exports.getHotelsByNameAndStars = getHotelsByNameAndStars;
-
-function getHoteles(params) {
-
-    let { name, estrellas } = params;
-    return new Promise((resolve, reject) => {
-
-        let result = data;
-
-        if (name && name.length > 3) {
-            let regExpStrign = `${name.toLowerCase()}`;
-            result = result.filter((elem) => new RegExp(regExpStrign).test(elem.name.toLowerCase()));
-        };
-
-        if (estrellas) {
-
-            result = result.filter(elem => {
-                let stars = estrellas.split(",");
-
-                for (let i in stars) {
-                    if (elem.stars === parseInt(stars[i])) {
-                        return true
-                    }
-                }
-                return false
-
-            });
-        }
-
-        if (!name && !estrellas) {
-            result = {};
-        }
-
-        (resolve(result))
-    });
-}
 
 function getHotelsByName(req, res) {
     let hotelName = req.params.hotelName;
